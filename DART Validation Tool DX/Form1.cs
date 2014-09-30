fafe8effa9180eb470b7961993194a48fe152c1a
Shuyi <sdjksdafji@gmail.com>
2014-09-30 14:52:23 -0700

    moved log to a seperate file

--- DART Validation Tool DX/Form1.cs
+++ DART Validation Tool DX/Form1.cs
@@ -104,7 +104,8 @@ namespace DART_Validation_Tool_DX
 						compareDataSeires.BeginGetDataSeries(gfsServerInfo, metricsBox.EditValue.ToString(), item.ToString(), false, this);
 						compareDataSeires.BeginGetDataSeries(osiServerInfo, metricsBox.EditValue.ToString(), item.ToString(), true, this);
 					}
-					Console.WriteLine("all");}
+					Console.WriteLine("all");
+				}
 				else
 				{
 					this.resultChart.Visible = false;
@@ -412,64 +413,4 @@ namespace DART_Validation_Tool_DX
 		}
 	}
 
-	public static class LogInfo
-	{
-		private static Object logLock = new Object();
-
-		private static String GetLogPath()
-		{
-			String path = @"log";
-			if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-			path += @"\log.txt";
-			return path;
-		}
-
-		public static void WriteComparisonToLog(String key, String osiServerName, String gfsServerName,
-			List<Tuple<DateTime, String, String>> diffList, int osiCount, int gfsCount)
-		{
-			String path = GetLogPath();
-			// This text is always added, making the file longer over time 
-			// if it is not deleted. 
-			lock (logLock)
-			{
-				using (StreamWriter sw = File.AppendText(path))
-				{
-					if (diffList.Count == 0)
-					{
-						sw.WriteLine("-- Matched --: { key = " + key + "}\t" + osiCount + " data in " + gfsServerName + " and " +
-						             osiServerName);
-					}
-					else
-					{
-						sw.WriteLine("!! Unmatched !!: { key = " + key + "}\t" + diffList.Count + " data in " + gfsServerName + " and " +
-						             osiServerName);
-						sw.WriteLine("\tSamples (up to 5):");
-						int i = 0;
-						foreach (Tuple<DateTime, String, String> tuple in diffList)
-						{
-							sw.WriteLine("\tTime: " + tuple.Item1 + "\t" + gfsServerName + ": " + tuple.Item2 + " instances\t" +
-							             osiServerName + ": " + tuple.Item3 + " instances");
-							if (++i >= 5)
-							{
-								break;
-							}
-						}
-					}
-				}
-			}
-		}
-
-		public static void WriteExceptionToLog(Exception ex)
-		{
-			String path = GetLogPath();
-			// This text is always added, making the file longer over time 
-			// if it is not deleted. 
-			lock (logLock)
-			{
-				using (StreamWriter sw = File.AppendText(path))
-				{
-					sw.WriteLine(ex.ToString());
-				}
-			}
-		}
-	}}
+}
